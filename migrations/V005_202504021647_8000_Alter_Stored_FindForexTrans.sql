c60c4776b4bfa64a6bcaa5f673a6c685c5f33871
Alfredo Rodriguez <alfredo.rodriguez@scisa.com.mx>
2025-04-02 16:49:14 -0600

    Create new script alter sp FindForexTrans

---
+++ migrations/V005_202504021647_8000_Alter_Stored_FindForexTrans.sql
@@ -0,0 +1,26 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author, Alfredo Rodriguez>
+-- Create date: <Create Date, 18/03/2025>
+-- Description:	<Description, Buscar operacion>
+-- =============================================
+ALTER PROCEDURE FindForexTranPrc
+AS
+BEGIN
+	SELECT ForexTransID, ContractID FROM TraForexTrans
+END
+GO
